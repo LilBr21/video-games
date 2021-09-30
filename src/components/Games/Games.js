4de3ce85c8af66f87921d369922d410ccd224706
Halina <halina.syposz@gmail.com>
2021-09-30 21:14:12 +0200

    main games fetched

--- src/components/Games/Games.js
+++ src/components/Games/Games.js
@@ -6,16 +6,21 @@ const Games = () => {
     const {games} = useGames();
 
     return (
-        <div>
-            <h2>Must play:</h2>
-        <div>
-        {games.map(game => (
-            <div key={game.id}>
-                <h3>{game.name}</h3>
-                <img className="game-img" src={game.background_image} alt="game" />
+        <div className="games-container">
+            <h2 className="games-title">Our games:</h2>
+            <div>
+            {games.map(game => (
+                <div className="game-container" key={game.id}>
+                    <h3 className="game-name">{game.name}</h3>
+                    <p className="game-rating">rating: {game.rating}</p>
+                    <img 
+                        className="game-img" 
+                        src={game.background_image} 
+                        alt="game" 
+                    />
+                </div>
+            ))}
             </div>
-        ))}
-        </div>
         </div>
     )
 }
--- src/components/Games/useGames.js
+++ src/components/Games/useGames.js
@@ -8,7 +8,8 @@ const useGames = () => {
       const [games, setGames] = useState([])
     
       const fetchGames = () => {
-        fetch('https://rawg.io/api/collections/must-play/games?key=2989b3e6293c4ef68887b2acde1f6efa')
+        //fetch('https://rawg.io/api/collections/must-play/games?key=2989b3e6293c4ef68887b2acde1f6efa')
+        fetch('https://api.rawg.io/api/games?key=2989b3e6293c4ef68887b2acde1f6efa&page_size=20')
         .then(resp => resp.json())
         .then(({results}) => setGames(results))
       }
