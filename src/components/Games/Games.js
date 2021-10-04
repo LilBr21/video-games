bbe3266c3c8c27b415681bf9dd273e24eaa22d35
Halina <halina.syposz@gmail.com>
2021-10-04 18:43:34 +0200

    game styles added

--- src/components/Games/Games.js
+++ src/components/Games/Games.js
@@ -6,18 +6,21 @@ const Games = () => {
     const {games} = useGames();
 
     return (
-        <div className="games-container">
-            <h2 className="games-title">All games:</h2>
+        <div className="game-part-container">
             <div>
+                <h2 className="games-title">All games:</h2>
+            </div>
+            <div className="games-container">
             {games.map(game => (
                 <div className="game-container" key={game.id}>
-                    <h3 className="game-name">{game.name}</h3>
-                    <p className="game-rating">rating: {game.rating}</p>
                     <img 
                         className="game-img" 
                         src={game.background_image} 
                         alt="game" 
                     />
+                    <h3 className="game-name">{game.name}</h3>
+                    <p className="game-rating">rating: {game.rating}</p>
+                    <button className="more-btn">More</button>
                 </div>
             ))}
             </div>
