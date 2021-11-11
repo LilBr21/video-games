ca4709bd7d22e23b796325eca14b6add314e0d02
Halina <halina.syposz@gmail.com>
2021-11-11 16:25:06 +0100

    not important comments deleted

--- src/store/prices.js
+++ src/store/prices.js
@@ -2,24 +2,11 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const initialPriceState = {allPrices: []};
 
-// const generateRandomPrice = (min, max) => {
-//     return (
-//         Math.floor(Math.random() * (max - min + 1) + min)
-//     )
-// }
-
 const priceSlice = createSlice({
     name: 'price',
     initialState: initialPriceState,
     reducers: {
         createPrice(state, action) {
-                // for (let priceIndex = 0; priceIndex < state.allPrices.length; priceIndex++) {
-                //     if (state.allPrices[priceIndex].id === action.payload.chosenId) {
-                //         state.activePrice = state.allPrices[priceIndex].price;
-                //     } else {
-                    //let firstPrice = action.payload.randomPrice;
-                    //let newPair = {id: action.payload.chosenId, price: generateRandomPrice()};
-                    console.log('shot')
                     state.allPrices = [...state.allPrices, action.payload];
                 }
             }
